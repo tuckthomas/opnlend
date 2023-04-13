bccbf6ae529ab04d98f3573ea22c51c7e12a8f92
Tucker Thomas Olson <90152609+tuckthomas@users.noreply.github.com>
2023-04-12 20:14:58 -0400

    Add files via upload
    Created additional and defined existing roles.

--- users/models.py
+++ users/models.py
@@ -3,16 +3,21 @@ from django.db import models
 
 class User(AbstractUser):
     USER_TYPE_CHOICES = [
+        #Sales Roles
         ('LOAN_OFFICER', 'Loan Officer'),
-        ('CREDIT_ANALYST', 'Credit Analyst'),
-        ('UNDERWRITER', 'Underwriter'),
-        ('PORTFOLIO_MANAGER', 'Portfolio Manager'),
-        ('CHIEF_CREDIT_OFFICER', 'Chief Credit Officer'),
         ('BUSINESS_DEVELOPMENT_OFFICER', 'Business Development Officer'),
+        #Credit Administration Roles
+        ('COMMERCIAL_CREDIT_ANALYST', 'Commercial Credit Analyst'),
+        ('COMMERCIAL_UNDERWRITER', 'Commercial Underwriter'),
+        ('COMMERCIAL_LOAN_ADMINISTRATOR', 'Commercial Loan Administrator'),
+        ('COMMERCIAL_PORTFOLIO_MANAGER', 'Commercial Portfolio Manager'),
+        #Loan Operations Roles
         ('LOAN_OPERATIONS_SPECIALIST', 'Loan Operations Specialist'),
+        #Executive Roles
+        ('CHIEF_CREDIT_OFFICER', 'Chief Credit Officer'),
         ('CHIEF_FINANCIAL_OFFICER', 'Chief Financial Officer'),
         ('CHIEF_EXECUTIVE_OFFICER', 'Chief Executive Officer'),
-        ('LOAN_ADMINISTRATOR', 'Loan Administrator'),
+        ('CHIEF_LENDING_OFFICER', 'Chief Lending Officer'),
     ]
 
     user_type = models.CharField(max_length=30, choices=USER_TYPE_CHOICES)
