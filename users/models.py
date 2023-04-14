610fa7135a53bf3ad974a050e0c7f596e8528019
Tucker Thomas Olson <90152609+tuckthomas@users.noreply.github.com>
2023-04-14 01:29:23 -0400

    Add files via upload

--- users/models.py
+++ users/models.py
@@ -1,7 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
-class User(AbstractUser):
+class CustomAbstractUser(AbstractUser):
+    groups = models.ManyToManyField(Group, related_name='custom_user_set', blank=True)
+    user_permissions = models.ManyToManyField(Permission, related_name='custom_user_set', blank=True)
+
+class User(CustomAbstractUser):
     USER_TYPE_CHOICES = [
         #Sales Roles
         ('LOAN_OFFICER', 'Loan Officer'),
